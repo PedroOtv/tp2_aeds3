70843721081d81156ae68c0d2585d3ecc5364ede
LeuMorreu <106126894+LeuMorreu@users.noreply.github.com>
2022-10-06 23:37:23 -0300

    Add files via upload

--- vetorProduto.py
+++ vetorProduto.py
@@ -2,19 +2,28 @@
 from Produto import Produto
 
 class VetorProduto(Produto):
-    def__init__(self):
+    def __init__(self):
         self.Produtos = []
 
     def addProduto(self, Produto):
         self.Produtos.append(Produto)
     
-    def buscarProdutoNome(self, nome)
+    def buscarProdutoNome(self, nome):
         for p in self.Produtos:
             if nome == p.nome:
                 print("Produto encontrado\n")
                 p.indentificacao()
                 return p
         print("Produto não encontrado\n")
+        return NONE
+
+    def buscarProdutoCodigo(self, codigo):
+        for p in self.Produtos:
+            if codigo == p.codigo:
+                print("Produto encotrado\n")
+                p.indentificacao()
+                return p
+        print("Produto não encotrado\n")
         return NONE
 
     def listaProdutos(self):
@@ -25,5 +34,3 @@ class VetorProduto(Produto):
     def removerProduto(self, Produto, quantidadeVendida):
         p = buscarProdutoNome(Produto)
         p.quantidade = p.quantidade - quantidadeVendida
-
-    
